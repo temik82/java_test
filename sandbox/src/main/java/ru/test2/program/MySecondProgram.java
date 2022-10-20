b1670475d185aff032c068907a5e242cb3dd739a
temik82 <testof1982@gmail.com>
2022-10-20 18:17:35 +0300

    Функция и метод дистанции между точками

---
+++ sandbox/src/main/java/ru/test2/program/MySecondProgram.java
@@ -0,0 +1,21 @@
+package ru.test2.program;
+
+public class MySecondProgram {
+  public static void main(String[] args) {
+
+    Point p1 = new Point(1, 2);
+    Point p2 = new Point(2, 4);
+    Point p = new Point(1, 2, 2, 4);
+
+
+    //дистанция между точками при использовании функции
+    System.out.println("расстояния между точками 1" + "(" + p1.x1 + "," + p1.y1 + ")" + " и " + "(" + p2.x2 + "," + p2.y2 + ")" + " = " + distance(p1, p2));
+    //дистанция между точками при использовании метода
+    System.out.println("расстояния между точками 2" + "(" + p.x1 + "," + p.y1 + ")" + " и " + "(" + p.x2 + "," + p.y2 + ")" + " = " + p.distance());
+
+  }
+
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt((p2.x2 - p1.x1) * (p2.x2 - p1.x1) + (p2.y2 - p1.y1) * (p2.y2 - p1.y1));
+  }
+}
---
+++ sandbox/src/main/java/ru/test2/program/Point.java
@@ -0,0 +1,28 @@
+package ru.test2.program;
+
+public class Point {
+  public double x1;
+  public double y1;
+  public double x2;
+  public double y2;
+
+  public Point(double x, double y) {
+    this.x1 = x;
+    this.x2 = x;
+    this.y1 = y;
+    this.y2 = y;
+
+  }
+
+  public Point(double x1, double y1, double x2, double y2) {
+    this.x1 = x1;
+    this.x2 = x2;
+    this.y1 = y1;
+    this.y2 = y2;
+  }
+
+  public double distance() {
+    return Math.sqrt((this.x2 - this.x1) * (this.x2 - this.x1) + (this.y2 - this.y1) * (this.y2 - this.y1));
+  }
+
+}
