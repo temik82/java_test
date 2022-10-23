a336db0105411a851dac0c19fd1acb0c5c5f0a2d
temik82 <testof1982@gmail.com>
2022-10-24 01:06:31 +0300

    Создание объекта ContactData

---
+++ addressbook-web-tests/src/test/java/ru/test3/addressbook/ContactData.java
@@ -0,0 +1,59 @@
+package ru.test3.addressbook;
+
+import java.util.Objects;
+
+public final class ContactData {
+  private final String firstName;
+  private final String lastNane;
+  private final String phone;
+  private final String email;
+
+  public ContactData(String firstName, String lastNane, String phone, String email) {
+    this.firstName = firstName;
+    this.lastNane = lastNane;
+    this.phone = phone;
+    this.email = email;
+  }
+
+  public String firstName() {
+    return firstName;
+  }
+
+  public String lastNane() {
+    return lastNane;
+  }
+
+  public String phone() {
+    return phone;
+  }
+
+  public String email() {
+    return email;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == this) return true;
+    if (obj == null || obj.getClass() != this.getClass()) return false;
+    var that = (ContactData) obj;
+    return Objects.equals(this.firstName, that.firstName) &&
+            Objects.equals(this.lastNane, that.lastNane) &&
+            Objects.equals(this.phone, that.phone) &&
+            Objects.equals(this.email, that.email);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(firstName, lastNane, phone, email);
+  }
+
+  @Override
+  public String toString() {
+    return "ContactData[" +
+            "firstName=" + firstName + ", " +
+            "lastNane=" + lastNane + ", " +
+            "phone=" + phone + ", " +
+            "email=" + email + ']';
+  }
+
+}
