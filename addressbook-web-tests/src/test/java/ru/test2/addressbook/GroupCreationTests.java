5a874d1cf16e64d08fe875b4c50dd239f6e0b13e
temik82 <testof1982@gmail.com>
2022-10-23 20:25:07 +0300

    Редактирование метода test

--- addressbook-web-tests/src/test/java/ru/test2/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/test2/addressbook/GroupCreationTests.java
@@ -17,11 +17,6 @@ public class GroupCreationTests {
     System.setProperty("webdriver.chrome.driver", "/Users/apple2/Documents/driver/chromedriver");
     wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-
-  }
-
-  @Test
-  public void testGroupCreation() throws Exception {
     wd.get("http://localhost/addressbook/group.php");
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys("admin");
@@ -29,6 +24,11 @@ public class GroupCreationTests {
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.xpath("//input[@value='Login']")).click();
+
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
     wd.findElement(By.linkText("groups")).click();
     wd.findElement(By.linkText("groups")).click();
     wd.findElement(By.name("new")).click();
