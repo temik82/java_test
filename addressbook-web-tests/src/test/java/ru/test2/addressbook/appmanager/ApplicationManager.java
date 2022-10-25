efbfea969ede3a8e227db9b893082834e1e1b07a
temik82 <testof1982@gmail.com>
2022-10-26 02:09:08 +0300

    рефакторинг и добавлен общий базовый класс HelperBase

--- addressbook-web-tests/src/test/java/ru/test2/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/test2/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,6 @@
 package ru.test2.addressbook.appmanager;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -39,14 +38,7 @@ public class ApplicationManager {
     }
   }
 
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
+
 
   public GroupHelper getGroupHelper() {
     return groupHelper;
--- addressbook-web-tests/src/test/java/ru/test2/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/test2/addressbook/appmanager/SessionHelper.java
@@ -3,19 +3,17 @@ package ru.test2.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class SessionHelper {
-  private WebDriver wd;
+public class SessionHelper extends HelperBase{
+
 
   public SessionHelper(WebDriver wd) {
 
-    this.wd = wd;
+    super(wd);
   }
-  public void login(String login, String password) {
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(login);
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
+  public void login(String username, String password) {
+    type(By.name("user"),username);
+    type(By.name("pass"),password);
+
+    click(By.xpath("//input[@value='Login']"));
   }
 }
