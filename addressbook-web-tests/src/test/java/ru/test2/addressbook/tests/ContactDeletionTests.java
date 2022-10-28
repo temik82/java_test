7f73bf4e60ee2fb4a0489dc237869ac80626b43f
temik82 <testof1982@gmail.com>
2022-10-29 01:03:45 +0300

    Рефакторинг кода

--- addressbook-web-tests/src/test/java/ru/test2/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/test2/addressbook/tests/ContactDeletionTests.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 
 public class ContactDeletionTests extends TestBase {
   @Test
-  public void testContactDeletion (){
+  public void testContactDeletion() {
     app.getNavigationHelper().goToContactPage();
     app.getContactHelper().selectContact();
     app.getContactHelper().deleteSelectedContact();
