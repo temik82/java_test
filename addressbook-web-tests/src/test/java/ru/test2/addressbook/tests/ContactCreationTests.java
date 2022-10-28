a79328c4d2f3e0bf96fbadcb7365e0c5faae6322
temik82 <testof1982@gmail.com>
2022-10-29 00:22:39 +0300

    Изменение в ContactCreationTests

--- addressbook-web-tests/src/test/java/ru/test2/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/test2/addressbook/tests/ContactCreationTests.java
@@ -10,6 +10,7 @@ public class ContactCreationTests extends TestBase {
 
   @Test
   public void testContactCreation() throws Exception {
+    app.getNavigationHelper().goToContactPage();
     app.getContactHelper().initContactCreation();
     app.getContactHelper().fillContactData(new ContactData("Petr", "Petrov", "89777777771", "test1@test.ru"));
     app.getContactHelper().submit();
