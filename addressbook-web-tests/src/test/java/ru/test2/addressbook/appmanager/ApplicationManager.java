240937945452a53d3cf046d90c7f75809b221175
temik82 <testof1982@gmail.com>
2022-10-31 18:49:18 +0300

    Добавлен класс Equality

--- addressbook-web-tests/src/test/java/ru/test2/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/test2/addressbook/appmanager/ApplicationManager.java
@@ -28,11 +28,11 @@ public class ApplicationManager {
 
 
   public void init() {
-    if (browser == BrowserType.CHROME) {
+    if (browser.equals(BrowserType.CHROME)) {
       wd = new ChromeDriver();
-    } else if (browser == BrowserType.FIREFOX) {
+    } else if (browser.equals(BrowserType.FIREFOX)) {
       wd = new FirefoxDriver();
-    } else if (browser == BrowserType.EDGE) {
+    } else if (browser.equals(BrowserType.EDGE)) {
       wd = new EdgeDriver();
     }
 
