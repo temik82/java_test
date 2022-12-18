1f2c5f63795d5ae39a5fabdd839115549f1d1f51
temik82 <testof1982@gmail.com>
2022-12-18 16:32:00 +0300

    Добавлен GithubTest

--- github-sample/src/test/java/ru/test4/test/GitHubTests.java
+++ github-sample/src/test/java/ru/test4/test/GitHubTests.java
@@ -1,13 +1,19 @@
 package ru.test4.test;
 
-import com.jcabi.github.Github;
-import com.jcabi.github.RtGithub;
+import com.google.common.collect.ImmutableBiMap;
+import com.jcabi.github.*;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
 
 public class GitHubTests {
   @Test
-  public void testCommits(){
-    Github github = new RtGithub(".. your OAuth token ..");
+  public void testCommits() throws IOException {
+    Github github = new RtGithub("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");
+    RepoCommits commits=github.repos().get(new Coordinates.Simple("temik82","java_test")).commits();
+    for (RepoCommit commit : commits.iterate(new ImmutableBiMap.Builder<String, String>().build())){
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
 
   }
 }
