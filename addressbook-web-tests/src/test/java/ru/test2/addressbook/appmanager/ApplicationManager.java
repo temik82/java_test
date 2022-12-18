b268ef3816f12a37582737b4f7480775d5f995be
temik82 <testof1982@gmail.com>
2022-12-19 01:42:55 +0300

    реализована возможность указывать платформу

--- addressbook-web-tests/src/test/java/ru/test2/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/test2/addressbook/appmanager/ApplicationManager.java
@@ -1,9 +1,6 @@
 package ru.test2.addressbook.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Capabilities;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
@@ -58,6 +55,7 @@ public class ApplicationManager {
   } else {
       DesiredCapabilities capabilities= new DesiredCapabilities();
       capabilities.setBrowserName(browser);
+      capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "Mac OS")));
       wd=new RemoteWebDriver(new URL(properties.getProperty("selenium.server")),capabilities);
     }
 
