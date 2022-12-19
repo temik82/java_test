05bc75d6a9eebda6357a234ca9826fe48d36236d
temik82 <testof1982@gmail.com>
2022-12-19 11:52:58 +0300

    изменения в remoteproperties

--- addressbook-web-tests/src/test/java/ru/test2/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/test2/addressbook/appmanager/ApplicationManager.java
@@ -55,7 +55,7 @@ public class ApplicationManager {
   } else {
       DesiredCapabilities capabilities= new DesiredCapabilities();
       capabilities.setBrowserName(browser);
-      capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "Mac OS")));
+    //  capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "Mac OS")));
       wd=new RemoteWebDriver(new URL(properties.getProperty("selenium.server")),capabilities);
     }
 
