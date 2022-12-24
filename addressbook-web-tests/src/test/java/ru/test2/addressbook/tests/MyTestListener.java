e7de3e6b3fe58b759297c20e05494cd752f12914
temik82 <testof1982@gmail.com>
2022-12-24 17:56:08 +0300

    изменения в myTestListerner

--- addressbook-web-tests/src/test/java/ru/test2/addressbook/tests/MyTestListener.java
+++ addressbook-web-tests/src/test/java/ru/test2/addressbook/tests/MyTestListener.java
@@ -3,58 +3,96 @@ package ru.test2.addressbook.tests;
 import io.qameta.allure.Attachment;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
+import org.testng.ITestNGMethod;
 import org.testng.ITestResult;
 import ru.test2.addressbook.appmanager.ApplicationManager;
+import org.testng.internal.IResultListener2;
 
-public class MyTestListener implements ITestListener {
+public class MyTestListener implements IResultListener2 {
+  @Override
+  public void onConfigurationSuccess(ITestResult tr) {
+
+  }
+
+  @Override
+  public void onConfigurationSuccess(ITestResult tr, ITestNGMethod tm) {
+
+  }
+
+  @Override
+  public void onConfigurationFailure(ITestResult tr) {
+
+  }
+
+  @Override
+  public void onConfigurationFailure(ITestResult tr, ITestNGMethod tm) {
+
+  }
+
+  @Override
+  public void onConfigurationSkip(ITestResult tr) {
+
+  }
+
+  @Override
+  public void onConfigurationSkip(ITestResult tr, ITestNGMethod tm) {
+
+  }
+
+  @Override
+  public void beforeConfiguration(ITestResult tr) {
+
+  }
+
+  @Override
+  public void beforeConfiguration(ITestResult tr, ITestNGMethod tm) {
+
+  }
 
   @Override
   public void onTestStart(ITestResult result) {
-    ITestListener.super.onTestStart(result);
-    ApplicationManager app=(ApplicationManager) result.getTestContext().getAttribute("app");
-    saveScreenshot(app.takeScreenShot());
+
   }
 
   @Override
   public void onTestSuccess(ITestResult result) {
-    ITestListener.super.onTestSuccess(result);
-    ApplicationManager app=(ApplicationManager) result.getTestContext().getAttribute("app");
-    saveScreenshot(app.takeScreenShot());
+
   }
 
   @Override
   public void onTestFailure(ITestResult result) {
-
     ApplicationManager app=(ApplicationManager) result.getTestContext().getAttribute("app");
-    saveScreenshot(app.takeScreenShot());
+    makeScreenshot(app.takeScreenShot());
+
   }
   @Attachment(value = "Page screenshot", type = "image/png")
-  public byte[] saveScreenshot(byte[] screenShot) {
+  public byte[] makeScreenshot(byte[] screenShot) {
     return screenShot;
   }
 
+
+
   @Override
   public void onTestSkipped(ITestResult result) {
-    ITestListener.super.onTestSkipped(result);
   }
 
   @Override
   public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-    ITestListener.super.onTestFailedButWithinSuccessPercentage(result);
+
   }
 
   @Override
   public void onTestFailedWithTimeout(ITestResult result) {
-    ITestListener.super.onTestFailedWithTimeout(result);
+
   }
 
   @Override
   public void onStart(ITestContext context) {
-    ITestListener.super.onStart(context);
+
   }
 
   @Override
   public void onFinish(ITestContext context) {
-    ITestListener.super.onFinish(context);
+
   }
 }
