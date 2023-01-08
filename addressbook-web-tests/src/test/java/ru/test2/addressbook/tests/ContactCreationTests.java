a4fbb283f9826c9be5f88f1fb9b5100e458e43aa
temik82 <testof1982@gmail.com>
2023-01-09 00:22:01 +0300

    реформат кода

--- addressbook-web-tests/src/test/java/ru/test2/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/test2/addressbook/tests/ContactCreationTests.java
@@ -89,6 +89,7 @@ public class ContactCreationTests extends TestBase {
     Contacts after = app.db().contacts();
 
 
+
     assertThat(after, equalTo(
             before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
 
